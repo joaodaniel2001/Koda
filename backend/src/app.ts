c3a462fbe74c75fdfe6788fd59fb19b05fe84cb0
Joao Daniel <joaodaniel2al@gmail.com>
2026-04-28 19:41:07 -0300

    feat: implement authentication routes and error handling middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,6 +6,8 @@ import chatRoutes from "./routes/chatRoutes"
 import messageRoutes from "./routes/messageRoutes"
 import userRoutes from "./routes/userRoutes"
 
+import { errorHandler } from "./middleware/errorHandler"
+
 const app = express()
 
 app.use(express.json())
@@ -15,5 +17,7 @@ app.use("/api/auth", authRoutes)
 app.use("/api/chats", chatRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
+
+app.use(errorHandler)
 
 export default app
--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -3,6 +3,26 @@ import { getAuth } from "@clerk/express"
 import { User } from "../models/User"
 import { requireAuth } from "@clerk/express"
 
+export type AuthRequest = Request & {
+    userId?: string;
+}
+
 export const protectRoute = [
-    requireAuth()
+    requireAuth(),
+    async (req:AuthRequest, res:Response, next:NextFunction) => {
+
+        try {
+            const { userId: clerkId } = getAuth(req)
+            if (!clerkId) return res.status(401).json({ message: "Unauthorized - invalid token" })
+            
+            const user = await User.findOne({ clerkId })
+            if (!user) return res.status(404).json({ message: "User not found!" })
+            
+            req.userId = user._id.toString()
+            next()
+        } catch (error) {
+            console.error(`Error in protectRoute middleware: ${error}`)
+            res.status(500).json({ message: "Internal server error..." })
+        }
+    }
 ]
