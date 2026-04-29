5aa9088b4d2a9ee2a820a96b92ec57ee6f92ab32
Joao Daniel <joaodaniel2al@gmail.com>
2026-04-28 22:11:18 -0300

    feat: enhance chat creation logic with validation and include last message in chat response

--- backend/src/controllers/chatController.ts
+++ backend/src/controllers/chatController.ts
@@ -1,6 +1,7 @@
 import type { NextFunction, Response } from "express";
 import type { AuthRequest } from "../middleware/auth";
 import { Chat } from "../models/Chat";
+import { Types } from "mongoose";
 
 export async function getChats(req: AuthRequest, res: Response, next: NextFunction) {
     
@@ -17,9 +18,10 @@ export async function getChats(req: AuthRequest, res: Response, next: NextFuncti
 
             return {
                 _id: chat._id,
-                participant: otherParticipant,
+                participant: otherParticipant ?? null,
+                lastMessage: chat.lastMessage,
                 lastMessageAt: chat.lastMessageAt,
-                createdAt: chat.createdAt
+                createdAt: chat.createdAt,
             }
         })
 
@@ -35,6 +37,17 @@ export async function getOrCreateChat(req: AuthRequest, res: Response, next: Nex
     try {
         const userId = req.userId
         const { participantId } = req.params
+
+        if (!participantId) {
+            res.status(400).json({ message: "Participant ID is required" })
+            return
+        }
+
+
+        if (userId === participantId) {
+            res.status(400).json({ message: "Cannot create chat with yourself!" })
+            return
+        }
 
         let chat = await Chat.findOne({
             participants: {$all: [userId, participantId]}
