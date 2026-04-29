79fafc0d7e44c028a167d09dc7b95d8eb6d320ae
Joao Daniel <joaodaniel2al@gmail.com>
2026-04-29 06:43:04 -0300

    feat(web): initialize Next.js project with TypeScript and Tailwind CSS
    - Added package.json for project dependencies and scripts.
    - Created postcss.config.mjs for Tailwind CSS integration.
    - Implemented proxy.ts for Clerk middleware.
    - Added SVG assets for UI components.
    - Configured TypeScript with tsconfig.json for strict type checking and module resolution.

---
+++ web/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-screen m-full justify-center items-center flex">
+      <h1>Hello world!</h1>
+    </div>
+  );
+}
